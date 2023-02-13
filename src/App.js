252081742d32eea1b263c1f58a3b63495715dc38
SS_11_RED <sssavio11@gmail.com>
2023-02-13 11:09:42 +0530

    added farms

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import About from './components/About';
+import Farms from './components/Farms';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
       <Routes>
         <Route path="/" >
           <Route path="about" element={<About />} />
+        </Route>
+         <Route path="/" >
+          <Route path="farms" element={<Farms />} />
         </Route>
       </Routes>
     </BrowserRouter>
