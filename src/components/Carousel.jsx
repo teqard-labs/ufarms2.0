a61956a1c20aa972ee85aa0874ada18f046f5444
josephgeorgepadayatty <josephgeorgepadayatty@gmail.com>
2023-02-13 22:56:28 +0530

    added flowbite

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,17 +1,35 @@
 import React from "react";
-const Carousel = () => {
+import { Carousel } from "flowbite-react";
+const Carousel1 = () => {
     return ( 
 
-        <div className="relative w-full flex gap-4 py-6 overflow-x-auto">
-	<img className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500" src="https://source.unsplash.com/random/241x361/?1" alt="Image 1" />
-	<img className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500" src="https://source.unsplash.com/random/241x361/?2" alt="Image 2" />
-	<img className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500" src="https://source.unsplash.com/random/241x361/?3" alt="Image 3" />
-	<img className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500" src="https://source.unsplash.com/random/241x361/?4" alt="Image 4" />
-	<img className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500" src="https://source.unsplash.com/random/241x361/?5" alt="Image 5" />
-</div>
+		<div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+		<Carousel>
+		  <img
+			src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+			alt="..."
+		  />
+		  <img
+			src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+			alt="..."
+		  />
+		  <img
+			src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+			alt="..."
+		  />
+		  <img
+			src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+			alt="..."
+		  />
+		  <img
+			src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+			alt="..."
+		  />
+		</Carousel>
+	  </div>
      );
 }
  
-export default Carousel;
+export default Carousel1;
 
 
