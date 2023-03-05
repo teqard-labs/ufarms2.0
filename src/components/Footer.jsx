0c3987690af2c3da1b09ba6b38f4ec07ce021a2f
sidharthR22 <114337068+sidharthR22@users.noreply.github.com>
2023-03-05 14:01:31 +0530

    updated contact info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,7 +30,7 @@ const Footer = () => {
                 </svg>
               </a>
               {/* Instagram */}
-              <a href="#link" className="ml-3 text-white">
+              <a href="https://www.instagram.com/ufarms.io/?igshid=YmMyMTA2M2Y%3D" className="ml-3 text-white">
                 <span className="sr-only">Instagram</span>
                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
                   <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
@@ -38,7 +38,7 @@ const Footer = () => {
                 </svg>
               </a>
               {/* Linkedin */}
-              <a href="#link" className="ml-3 text-white">
+              <a href="https://www.linkedin.com/company/ufarmsio/about/" className="ml-3 text-white">
                 <span className="sr-only">Linkedin</span>
                 <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-6 h-6" viewBox="0 0 24 24">
                   <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
@@ -70,17 +70,21 @@ const Footer = () => {
           {/* ::Address */}
           <div className="flex flex-col items-center col-span-2 px-4 pb-0 md:col-span-1 md:py-3 sm:items-start">
             <h3 className="py-1.5 md:py-4 text-xl text-white font-bold tracking-wide">Address</h3>
-            <p className="text-lg font-medium text-center md:w-48 sm:text-left md:text-xl">101 Paradise Road, 97460 Fancy Island REUNION</p>
+            <p className="text-lg font-medium text-center md:w-48 sm:text-left md:text-xl">Marangadan Puthussery Bldg,
+
+Nayathode (CIAL City 130),
+
+Angamaly P.O, Ernakulam, Kerala, India, PIN : 683572</p>
           </div>
           {/* ::Email */}
           <div className="flex flex-col items-center col-span-2 px-4 pb-0 md:col-span-1 md:py-3 sm:items-start">
-            <h3 className="py-1.5 md:py-4 text-xl text-white font-bold tracking-wide">Fancy Company</h3>
+            <h3 className="py-1.5 md:py-4 text-xl text-white font-bold tracking-wide">Email</h3>
             <p className="inline-flex justify-center text-sm font-medium text-white sm:justify-start">
               <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
               </svg>
-              <a href="#email">fancytailwind@paradise.com</a>
+              <a href="#email">info@teqardlabs.com</a>
             </p>
           </div>
           {/* ::Phone */}
@@ -90,7 +94,7 @@ const Footer = () => {
               <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
               </svg>
-              <span>209-217-2459</span>
+              <span>+91 9496516291, +91 9645546443</span>
             </p>
           </div>
         </div>
