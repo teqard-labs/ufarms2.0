4dc5831ed3f30c6a5049c06f2a6611d6e0da4908
josephgeorgepadayatty <josephgeorgepadayatty@gmail.com>
2023-03-17 14:54:46 +0530

    fixed mobile view in farms

--- src/components/Farms.jsx
+++ src/components/Farms.jsx
@@ -82,19 +82,20 @@ const Farms = () => {
       <div className="grid grid-cols-1 mt-20 sm:grid-cols-2">
         {products.map((product, index) => (
           <div className="text-black">
-            <div className="border-2 border-cyan-50 rounded-lg grid grid-cols-12 mx-2 my-2 bg-green-100">
-              <div className="col-span-full lg:col-span-4">
-                <img src={product.picture} alt="" className=" rounded-full md:right-10 right-0 md:h-[35vh] md:max-lg:h-[20vh] md:max-lg:right-0 md:w-[40vw] w-[40vw] h-[17vh] opacity-100 scale-75" />
+            <div className="grid grid-cols-12 mx-2 my-2 bg-green-100 border-2 rounded-lg border-cyan-50">
+              <div className="justify-center hidden col-start-7 col-span-full lg:col-span-4 md:inline">
+                <img src={product.picture} alt="" className=" rounded-full md:right-10 right-0 md:h-[35vh] md:max-lg:h-[20vh] md:max-lg:right-0 md:w-[40vw] w-[40vw] h-[17vh] opacity-100 scale-75 sm:left-0 sm:top-0" />
               </div>
-              <div className="p-6 col-span-full row-span-full lg:col-span-8 lg:p-10">
-                <div className='flex flex-col '>
-                  <div className="flex justify-start">
+              <div className="p-6 col-span-full row-span-full lg:col-span-8 lg:p-10 ">
+                <div className='flex flex-col'>
+                  <div className="justify-start hidden md:inline ">
                     <span className="py-1 text-lg rounded-full dark:bg-violet-400 dark:text-gray-900">{product.badge}</span>
                   </div>
-                  <h1 className="text-3xl font-semibold">{product.name}</h1>
+                  <h1 className="hidden text-3xl font-semibold md:inline">{product.name}</h1>
                   <p className="hidden text-lg text-gray-500 md:block lg:w-2/3">{product.description}</p>
-                  <Modal1 />
-                  <div className="flex items-center justify-between pt-2 mt-2">
+                  <div className='hidden md:inline'>
+                  <Modal1 /></div>
+                  <div className="items-center justify-between hidden pt-2 mt-2 md:inline">
                     <div className="flex space-x-2">
                       <svg height="24" version="1.1" width="24" xmlns="http://www.w3.org/2000/svg" xmlnsCc="http://creativecommons.org/ns#" xmlnsDc="http://purl.org/dc/elements/1.1/" xmlnsRdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
                         <g transform="translate(0 -1028.4)">
@@ -106,6 +107,29 @@ const Farms = () => {
                     </div>
                   </div>
                   {/* <span className="text-xs">3 min read</span> */}
+
+                  <div className="flex flex-col items-center justify-center md:hidden">
+                    <img src={product.picture} alt="" className=" w-[60vw] h-[30vh]  rounded-full items-center  md:hidden" />
+                    <div className="flex justify-start">
+
+                      <span className="py-1 text-lg rounded-full dark:bg-violet-400 dark:text-gray-900">{product.badge}</span>
+                    </div>
+                    <h1 className="text-3xl font-semibold">{product.name}</h1>
+                    <p className="hidden text-lg text-gray-500 md:block lg:w-2/3">{product.description}</p>
+                    <Modal1 />
+                    <div className="flex items-center justify-between pt-2 mt-2">
+                      <div className="flex space-x-2">
+                        <svg height="24" version="1.1" width="24" xmlns="http://www.w3.org/2000/svg" xmlnsCc="http://creativecommons.org/ns#" xmlnsDc="http://purl.org/dc/elements/1.1/" xmlnsRdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+                          <g transform="translate(0 -1028.4)">
+                            <path d="m12 0c-4.4183 2.3685e-15 -8 3.5817-8 8 0 1.421 0.3816 2.75 1.0312 3.906 0.1079 0.192 0.221 0.381 0.3438 0.563l6.625 11.531 6.625-11.531c0.102-0.151 0.19-0.311 0.281-0.469l0.063-0.094c0.649-1.156 1.031-2.485 1.031-3.906 0-4.4183-3.582-8-8-8zm0 4c2.209 0 4 1.7909 4 4 0 2.209-1.791 4-4 4-2.2091 0-4-1.791-4-4 0-2.2091 1.7909-4 4-4z" fill="#e74c3c" transform="translate(0 1028.4)" />
+                            <path d="m12 3c-2.7614 0-5 2.2386-5 5 0 2.761 2.2386 5 5 5 2.761 0 5-2.239 5-5 0-2.7614-2.239-5-5-5zm0 2c1.657 0 3 1.3431 3 3s-1.343 3-3 3-3-1.3431-3-3 1.343-3 3-3z" fill="#c0392b" transform="translate(0 1028.4)" />
+                          </g>
+                        </svg>
+                        <span className="self-center text-sm">{product.location}</span>
+                      </div>
+                    </div>
+                    {/* <span className="text-xs">3 min read</span> */}
+                  </div>
                 </div>
               </div>
             </div>
